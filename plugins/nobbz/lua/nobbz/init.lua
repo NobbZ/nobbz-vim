2172e8bfbbed7c7523e75ae057a86861fcdeb829
Norbert Melzer <timmelzer@gmail.com>
2024-06-16 16:27:53 +0200

    some commenting

--- plugins/nobbz/lua/nobbz/init.lua
+++ plugins/nobbz/lua/nobbz/init.lua
@@ -1,10 +1,10 @@
-require("nobbz.cmp")
-require("nobbz.git")
-require("nobbz.lsp")
-require("nobbz.lualine")
-require("nobbz.markdown")
-require("nobbz.misc")
-require("nobbz.telescope")
-require("nobbz.theme")
-require("nobbz.treesitter")
-require("nobbz.whichkey")
+require("nobbz.cmp")        -- foundations for completions
+require("nobbz.git")        -- set up neogit (kind of magit)
+require("nobbz.lsp")        -- LSP and related setup
+require("nobbz.lualine")    -- Set up the status bar at the bottom
+require("nobbz.markdown")   -- Set up markdown editing
+require("nobbz.misc")       -- miscelanous editor settings
+require("nobbz.telescope")  -- some fuzzy finders
+require("nobbz.theme")      -- how shall everything look like
+require("nobbz.treesitter") -- set up treesitter
+require("nobbz.whichkey")   -- set up whichkey, which provides help as you type
--- plugins/nobbz/lua/nobbz/misc.lua
+++ plugins/nobbz/lua/nobbz/misc.lua
@@ -1,16 +1,16 @@
-vim.o.mouse = "a"
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.o.ai = true
-vim.o.colorcolumn = "80,100,120"
-vim.o.signcolumn = "yes"
-vim.o.number = true
-vim.o.cursorline = true
-vim.o.cursorcolumn = true
+vim.o.mouse = "a"                -- allow mouse everywhere
+vim.o.tabstop = 2                -- \
+vim.o.softtabstop = 2            -- |
+vim.o.shiftwidth = 2             -- > Set tab behaviour
+vim.o.expandtab = true           -- /
+vim.o.autoindent = true          -- Auto indent
+vim.o.colorcolumn = "80,100,120" -- set column markers
+vim.o.signcolumn = "yes"         -- always show signcolumn
+vim.o.number = true              -- always show line numbers
+vim.o.cursorline = true          -- slightly color the line the cursor is on
+vim.o.cursorcolumn = true        -- slightly color the column the cursor is on
 
-vim.g.mapleader = " "
+vim.g.mapleader = " "            -- set `<leader>` to the space key
 
 -- Set up indent markers
 require("ibl").setup({
