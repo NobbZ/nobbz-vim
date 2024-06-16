0bd89566955e4bc3c109bd10712d0383dec015fb
Norbert Melzer <timmelzer@gmail.com>
2024-06-16 17:30:34 +0200

    Add oil configuration

--- plugins/nobbz/lua/nobbz/init.lua
+++ plugins/nobbz/lua/nobbz/init.lua
@@ -4,6 +4,7 @@ require("nobbz.lsp")        -- LSP and related setup
 require("nobbz.lualine")    -- Set up the status bar at the bottom
 require("nobbz.markdown")   -- Set up markdown editing
 require("nobbz.misc")       -- miscelanous editor settings
+require("nobbz.oil")        -- manage files as if it was a text buffer
 require("nobbz.telescope")  -- some fuzzy finders
 require("nobbz.theme")      -- how shall everything look like
 require("nobbz.treesitter") -- set up treesitter
---
+++ plugins/nobbz/lua/nobbz/oil.lua
@@ -0,0 +1,22 @@
+local function is_always_hidden(name)
+  if name == ".git" then
+    return true
+  end
+
+  return false
+end
+
+require("oil").setup({
+  columns = { "icon", "permissions", "size", "mtime" },
+  view_options = {
+    show_hidden = true,
+    is_always_hidden = is_always_hidden,
+  },
+})
+
+require("which-key").register({
+  o = {
+    name = "oil file explorer",
+    ["."] = { "<cmd>Oil<cr>", "open containing directory" },
+  },
+}, { prefix = "<leader>" })
