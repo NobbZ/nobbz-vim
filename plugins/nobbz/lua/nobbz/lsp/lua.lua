09551ad691f6bba00888fefbddae4eac1f720af5
Norbert Melzer <timmelzer@gmail.com>
2024-06-21 22:50:29 +0200

    find local plugins

--- plugins/nobbz/lua/nobbz/lsp/lua.lua
+++ plugins/nobbz/lua/nobbz/lsp/lua.lua
@@ -13,15 +13,20 @@ require("lspconfig").lua_ls.setup({
     local luarc_jsonc_exists = vim.fn.glob(path .. "/.luarc.jsonc") ~= ""
     if luarc_json_exists or luarc_jsonc_exists then return end
 
+    -- if there is a luarc.lua in the workspace root, import and merge.
+    local plugin_paths = vim.fn.glob(path .. "/plugins/*/lua")
+    for i, plugin_path in pairs(plugin_paths) do
+      plugin_paths[i] = vim.print(string.sub(plugin_path, -1, -4))
+    end
+    table.insert(plugin_paths, vim.env.VIMRUNTIME)
+
     -- Assume we are in a nvim config and configure appropriately to not warn on nvim globals
     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
       runtime = { version = "LuaJIT" },
       -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-        },
+        library = plugin_paths,
       },
     })
   end,
