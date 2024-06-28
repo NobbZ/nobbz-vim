87e94c2aa9f99baff98983a7d80c25d1225574aa
Norbert Melzer <timmelzer@gmail.com>
2024-06-28 07:43:36 +0200

    enable inlay hints for lua

--- plugins/nobbz/lua/nobbz/lsp/lua.lua
+++ plugins/nobbz/lua/nobbz/lsp/lua.lua
@@ -1,7 +1,7 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("lspconfig").lua_ls.setup({
-  on_attach = require("nobbz.lsp.keymap"),
+  on_attach = require("nobbz.lsp.attach"),
 
   -- this snippet is adopted from:
   -- https://github.com/neovim/nvim-lspconfig/blob/37f362ef42d1a604d332e8d3d7d47593852b4313/doc/server_configurations.md#lua_ls
@@ -15,7 +15,7 @@ require("lspconfig").lua_ls.setup({
 
     -- if there is a luarc.lua in the workspace root, import and merge.
     local plugin_paths = vim.print(vim.split(vim.fn.glob(path .. "/plugins/*/lua"), "\n"), { trimempty = true })
-    vim.iter(plugin_paths):map(function (plugin_path)
+    vim.iter(plugin_paths):map(function(plugin_path)
       return vim.print(string.sub(plugin_path, -1, -4))
     end)
     table.insert(plugin_paths, vim.env.VIMRUNTIME)
@@ -30,6 +30,10 @@ require("lspconfig").lua_ls.setup({
       },
     })
   end,
-  settings = { Lua = {} },
+  settings = {
+    Lua = {
+      hint = { enable = true, },
+    }
+  },
   capabilities = capabilities,
 })
