71a23501a9b6bbe0c1bf9606a276aabb285fb474
Norbert Melzer <timmelzer@gmail.com>
2024-06-28 08:51:13 +0200

    some types and docs

--- plugins/nobbz/lua/nobbz/lsp/keymap.lua
+++ plugins/nobbz/lua/nobbz/lsp/keymap.lua
@@ -1,6 +1,9 @@
 local wk = require("which-key")
 
-return function(_, _)
+---Registers the default keymap as intended for LSP powered buffers.
+---@param client vim.lsp.Client
+---@param buffer integer
+local function attach_keymap(client, buffer) ---@diagnostic disable-line:unused-local
   wk.register({
     name = "language server",
     ["gD"] = { vim.lsp.buf.declaration, "jump to declaratin" },
@@ -18,3 +21,5 @@ return function(_, _)
     ["<C-.>"] = { vim.lsp.buf.code_action, "show code actions" },
   })
 end
+
+return attach_keymap
