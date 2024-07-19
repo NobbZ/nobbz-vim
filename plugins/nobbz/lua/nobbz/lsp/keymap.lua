ca1ae727de3569b903f43b895b3528e49ebefd5b
Norbert Melzer <timmelzer@gmail.com>
2024-07-19 18:32:00 +0200

    migrate keymaps for LSP to new WK style

--- plugins/nobbz/lua/nobbz/lsp/keymap.lua
+++ plugins/nobbz/lua/nobbz/lsp/keymap.lua
@@ -1,24 +1,25 @@
 local wk = require("which-key")
 
+---Triggers a reformat of the current buffer when called
+local function format_buffer() vim.lsp.buf.format({ async = true, }) end
+
 ---Registers the default keymap as intended for LSP powered buffers.
 ---@param client vim.lsp.Client
 ---@param buffer integer
 local function attach_keymap(client, buffer) ---@diagnostic disable-line:unused-local
-  wk.register({
-    name = "language server",
-    ["gD"] = { vim.lsp.buf.declaration, "jump to declaratin", },
-    ["gd"] = { vim.lsp.buf.definition, "jump to definition", },
-    ["gt"] = { vim.lsp.buf.type_definition, "jump to type definition", },
-    ["gn"] = { vim.diagnostic.goto_next, "jump to next diagnostic message", },
-    ["gp"] = { vim.diagnostic.goto_prev, "jump to previous diagnostic message", },
-    ["h"] = { vim.lsp.buf.hover, "show hover info", },
-    ["s"] = { vim.lsp.buf.signature_help, "show signature help", },
-    ["r"] = { vim.lsp.buf.rename, "rename symbol", },
-    ["f"] = { function() vim.lsp.buf.format({ async = true, }) end, "format buffer", },
-  }, { prefix = "<leader>l", })
-
-  wk.register({
-    ["<C-.>"] = { vim.lsp.buf.code_action, "show code actions", },
+  wk.add({
+    { "<leader>l",   group = "language server", },
+    { "<leader>lg",  group = "goto", },
+    { "<leader>lgD", vim.lsp.buf.declaration,     desc = "jump to declaration", },
+    { "<leader>lgd", vim.lsp.buf.definition,      desc = "jump to definition", },
+    { "<leader>lgt", vim.lsp.buf.type_definition, desc = "jump to type definition", },
+    { "<leader>lgn", vim.diagnostic.goto_next,    desc = "jump to next diagnostic", },
+    { "<leader>lgp", vim.diagnostic.goto_prev,    desc = "jump to prev diagnostic", },
+    { "<leader>lh",  vim.lsp.buf.hover,           desc = "show hover info", },
+    { "<leader>ls",  vim.lsp.buf.signature_help,  desc = "show signature info", },
+    { "<leader>lr",  vim.lsp.buf.rename,          desc = "rename symbol", },
+    { "<leader>lf",  format_buffer,               desc = "format buffer", },
+    { "<c-.>",       vim.lsp.buf.code_action,     desc = "show code actions", },
   })
 end
 
