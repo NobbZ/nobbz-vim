44f0794a3c1e2d1ef27260be0689b4b9061fa093
Norbert Melzer <timmelzer@gmail.com>
2024-07-19 23:52:21 +0200

    update neovim

--- nvim.nix
+++ nvim.nix
@@ -10,15 +10,94 @@
   stylua,
   markdown-oxide,
   xclip,
+  utf8proc,
+  fetchurl,
+  gettext,
+  libiconv,
+  libvterm-neovim,
+  luajit,
+  msgpack-c,
+  tree-sitter,
+  rustPlatform,
 }: let
-  neovim-nightly = neovim-unwrapped.overrideAttrs {
+  # The building of `deps` and `overrides` later on are shamelessly borrowed
+  # from the neovim-nightly-overlay.
+  # TODO: generate an importable nix expression in update script
+  deps = lib.pipe ./deps.txt [
+    builtins.readFile
+    (lib.splitString "\n")
+    (map (builtins.match "([A-Z0-9_]+)_(URL|SHA256)[[:space:]]+([^[:space:]]+)[[:space:]]*"))
+    (lib.remove null)
+    (lib.flip builtins.foldl' {}
+      (acc: matches: let
+        name = lib.toLower (builtins.elemAt matches 0);
+        key = lib.toLower (builtins.elemAt matches 1);
+        value = lib.toLower (builtins.elemAt matches 2);
+      in
+        acc
+        // {
+          ${name} =
+            acc.${name}
+            or {}
+            // {
+              ${key} = value;
+            };
+        }))
+    (builtins.mapAttrs (lib.const fetchurl))
+  ];
+
+  overrides = {
+    gettext = gettext.overrideAttrs {
+      src = deps.gettext;
+    };
+
+    # pkgs.libiconv.src is pointing at the darwin fork of libiconv.
+    # Hence, overriding its source does not make sense on darwin.
+    libiconv = libiconv.overrideAttrs {
+      src = deps.libiconv;
+    };
+    libvterm-neovim = libvterm-neovim.overrideAttrs {
+      src = deps.libvterm;
+    };
+    lua = luajit;
+    msgpack-c = msgpack-c.overrideAttrs {
+      src = deps.msgpack;
+    };
+    tree-sitter = tree-sitter.override {
+      rustPlatform =
+        rustPlatform
+        // {
+          buildRustPackage = args:
+            rustPlatform.buildRustPackage (args
+              // {
+                src = deps.treesitter;
+                cargoHash = "sha256-U2YXpNwtaSSEftswI0p0+npDJqOq5GqxEUlOPRlJGmQ=";
+              });
+        };
+    };
+    treesitter-parsers = let
+      grammars = lib.filterAttrs (name: _: lib.hasPrefix "treesitter_" name) deps;
+    in
+      lib.mapAttrs'
+      (
+        name: value:
+          lib.nameValuePair
+          (lib.removePrefix "treesitter_" name)
+          {src = value;}
+      )
+      grammars;
+  };
+
+  neovim-nightly = (neovim-unwrapped.override overrides).overrideAttrs (old: {
     src = npins.neovim;
     version = npins.neovim.revision;
     patches = [];
+    buildInputs = (old.buildInputs or []) ++ [utf8proc];
     preConfigure = ''
+      ${old.preConfigure}
       sed -i cmake.config/versiondef.h.in -e "s/@NVIM_VERSION_PRERELEASE@/-dev-$version/"
     '';
-  };
+  });
 
   config = neovimUtils.makeNeovimConfig {
     plugins = builtins.attrValues ({
