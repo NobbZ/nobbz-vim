73c322dad99ca8a2f1074b6dff6b43efeabc9d94
Norbert Melzer <timmelzer@gmail.com>
2024-07-20 09:48:24 +0200

    refactor updater script
    This makes functionality to be on par with what there was before, to be
    able to make it easier to do what I actually want later on

--- bin/update-nvim.exs
+++ bin/update-nvim.exs
@@ -5,21 +5,62 @@ Mix.install([
   {:req, "~> 0.5.4"}
 ])
 
-"https://github.com/nix-community/neovim-nightly-overlay/raw/master/flake.lock"
-|> Req.get!()
-|> then(& &1.body)
-|> Jason.decode!()
-|> get_in(["nodes", "neovim-src", "locked"])
-|> tap(fn %{"owner" => owner, "repo" => repo, "rev" => rev} ->
-  "https://github.com/#{owner}/#{repo}/raw/#{rev}/cmake.deps/deps.txt"
-  |> Req.get!()
-  |> then(& &1.body)
-  |> then(&File.write("deps.txt", &1))
-end)
-|> then(fn %{"owner" => owner, "repo" => repo, "rev" => rev} ->
-  [
-    ["remove", "neovim"],
-    ["add", "--name", "neovim", "github", owner, repo, "--at", rev, "--branch", "master"]
-  ]
-end)
-|> Enum.each(&System.cmd("npins", &1))
+defmodule NeovimUpdater do
+  @upstream_lock "https://github.com/nix-community/neovim-nightly-overlay/raw/master/flake.lock"
+
+  defmodule Lock do
+    defstruct [:owner, :repo, :rev, :type]
+
+    def parse(raw) do
+      %{"owner" => owner, "repo" => repo, "rev" => rev, "type" => type} =
+        get_in(raw, ["nodes", "neovim-src", "locked"])
+
+      %__MODULE__{
+        owner: owner,
+        repo: repo,
+        rev: rev,
+        type: type
+      }
+    end
+  end
+
+  defmodule Deps do
+    def fetch!(lock) do
+      lock
+      |> build_url()
+      |> Req.get!()
+      |> NeovimUpdater.get_body()
+      |> then(&File.write("deps.txt", &1))
+    end
+
+    defp build_url(%{owner: owner, repo: repo, rev: rev}) do
+      "https://github.com/#{owner}/#{repo}/raw/#{rev}/cmake.deps/deps.txt"
+    end
+  end
+
+  def update() do
+    @upstream_lock
+    |> Req.get!()
+    |> get_body()
+    |> Jason.decode!()
+    |> Lock.parse()
+    |> tap(&Deps.fetch!/1)
+    |> build_npins_args()
+    |> run_npins()
+  end
+
+  defp build_npins_args(%{owner: owner, repo: repo, rev: rev}) do
+    [
+      ["remove", "neovim"],
+      ["add", "--name", "neovim", "github", owner, repo, "--at", rev, "--branch", "master"]
+    ]
+  end
+
+  def run_npins(args) do
+    Enum.each(args, &System.cmd("npins", &1))
+  end
+
+  def get_body(%{body: body}), do: body
+end
+
+NeovimUpdater.update()
