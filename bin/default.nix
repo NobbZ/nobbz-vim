4792cd383827e58e1ea4fda7849f511a029f0c7a
Norbert Melzer <timmelzer@gmail.com>
2024-08-12 20:34:23 +0200

    add script for updating neovim

--- bin/default.nix
+++ bin/default.nix
@@ -6,13 +6,16 @@
   }: {
     packages = {
       addPlugin = pkgs.callPackage ./add-plugin.nix {inherit self;};
+      updateNvim = pkgs.callPackage ./update-nvim.nix {inherit self;};
       updatePlugins = pkgs.callPackage ./update-plugins.nix {inherit self;};
     };
 
     apps.add-plugin.program = self'.packages.addPlugin;
+    apps.update-nvim.program = self'.packages.updateNvim;
     apps.update-plugins.program = self'.packages.updatePlugins;
 
     checks.addPlugin = self'.packages.addPlugin;
+    checks.updateNvim = self'.packages.updateNvim;
     checks.updatePlugins = self'.packages.updatePlugins;
   };
 }
