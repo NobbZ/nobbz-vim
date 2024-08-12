b8b5abba2733d0d6330bbde2af5222e326c3cca6
Norbert Melzer <timmelzer@gmail.com>
2024-08-12 18:10:26 +0200

    wire up scripts correctly in flake

--- bin/default.nix
+++ bin/default.nix
@@ -2,8 +2,13 @@
   perSystem = {self', pkgs, ...}: {
     packages = {
       addPlugin = pkgs.callPackage ./add-plugin.nix {inherit self;};
+      updatePlugins = pkgs.callPackage ./update-plugins.nix {inherit self;};
     };
 
     apps.add-plugin.program = self'.packages.addPlugin;
+    apps.update-plugins.program = self'.packages.updatePlugins;
+
+    checks.addPlugin = self'.packages.addPlugin;
+    checks.updatePlugins = self'.packages.updatePlugins;
   };
 }
