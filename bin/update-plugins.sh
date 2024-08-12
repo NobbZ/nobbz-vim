cc9c195123aca59dd56cc5d79cc4ed8a2f0ef868
Norbert Melzer <timmelzer@gmail.com>
2024-08-12 21:55:14 +0200

    fix update plugin script

--- bin/update-plugins.sh
+++ bin/update-plugins.sh
@@ -2,9 +2,9 @@
 
 pins="$(pwd)"/npins/sources.json
 
-plugins=($(jq -rS '.pins | keys[] | select(startswith("nvim-"))' "$pins"))
+mapfile -t plugins < <(jq -rS '.pins | keys[] | select(startswith("nvim-"))' "$pins")
 
-for p in ${plugins[@]}; do
+for p in "${plugins[@]}"; do
   printf 'updating "%s"\n' "$p"
   npins update "$p"
   if git diff --name-only | grep "$pins"; then
