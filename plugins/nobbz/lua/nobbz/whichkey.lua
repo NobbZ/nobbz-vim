13f9d2755bd7ba81ff3450985752a50ab2839636
Norbert Melzer <timmelzer@gmail.com>
2024-08-22 00:06:00 +0200

    add FPS nav

--- plugins/nobbz/lua/nobbz/whichkey.lua
+++ plugins/nobbz/lua/nobbz/whichkey.lua
@@ -4,3 +4,14 @@ vim.o.timeout = true
 vim.o.timeoutlen = 300
 
 wk.setup({})
+
+wk.add({
+  { "w",     "<Up>",    mode = { "v", "n", }, },
+  { "a",     "<Left>",  mode = { "v", "n", }, },
+  { "s",     "<Down>",  mode = { "v", "n", }, },
+  { "d",     "<Right>", mode = { "v", "n", }, },
+  { "<c-w>", "<Up>",    mode = "i", },
+  { "<c-a>", "<Left>",  mode = "i", },
+  { "<c-s>", "<Down>",  mode = "i", },
+  { "<c-d>", "<Right>", mode = "i", },
+})
