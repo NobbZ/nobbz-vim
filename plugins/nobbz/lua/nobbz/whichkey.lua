8b5d637c18595b1673e6f21556a77ea2dc952ec0
Norbert Melzer <timmelzer@gmail.com>
2024-08-26 23:05:41 +0200

    always use CTRL for lefthand navigation

--- plugins/nobbz/lua/nobbz/whichkey.lua
+++ plugins/nobbz/lua/nobbz/whichkey.lua
@@ -4,12 +4,8 @@ vim.o.timeoutlen = 300
 WK.setup({})
 
 WK.add({
-  { "w",     "<Up>",    mode = { "v", "n", }, },
-  { "a",     "<Left>",  mode = { "v", "n", }, },
-  { "s",     "<Down>",  mode = { "v", "n", }, },
-  { "d",     "<Right>", mode = { "v", "n", }, },
-  { "<c-w>", "<Up>",    mode = "i", },
-  { "<c-a>", "<Left>",  mode = "i", },
-  { "<c-s>", "<Down>",  mode = "i", },
-  { "<c-d>", "<Right>", mode = "i", },
+  { "<c-w>", "<Up>",    mode = { "v", "n", "i", }, },
+  { "<c-a>", "<Left>",  mode = { "v", "n", "i", }, },
+  { "<c-s>", "<Down>",  mode = { "v", "n", "i", }, },
+  { "<c-d>", "<Right>", mode = { "v", "n", "i", }, },
 })
