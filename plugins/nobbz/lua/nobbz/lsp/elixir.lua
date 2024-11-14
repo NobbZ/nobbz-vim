58462e6d5311f832196adfd8e1c1c532c987c8ea
Norbert Melzer <timmelzer@gmail.com>
2024-11-14 21:25:15 +0100

    switch to elixir-ls

--- plugins/nobbz/lua/nobbz/lsp/elixir.lua
+++ plugins/nobbz/lua/nobbz/lsp/elixir.lua
@@ -1,15 +1,7 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").nextls.setup({
+require("lspconfig").elixirls.setup({
   on_attach = require("nobbz.lsp.keymap"),
   capabilities = capabilities,
-  cmd = { "nextls", "--stdio", },
-  init_options = {
-    extensions = {
-      credo = { enable = true, },
-    },
-    experimental = {
-      completions = { enable = true, },
-    },
-  },
+  cmd = { "elixir-ls", },
 })
