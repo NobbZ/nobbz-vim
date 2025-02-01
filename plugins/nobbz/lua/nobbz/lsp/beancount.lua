2ae8aa51346308e73d357b7a92c7dc02790b801e
Norbert Melzer <timmelzer@gmail.com>
2025-02-01 14:18:58 +0100

    properly search project root and main file

--- plugins/nobbz/lua/nobbz/lsp/beancount.lua
+++ plugins/nobbz/lua/nobbz/lsp/beancount.lua
@@ -1,6 +1,13 @@
 local helpers = require("nobbz.lsp.helpers")
 
+-- find the root dir of the project by finding a .git folder
+root_dir = vim.fs.dirname(vim.fs.find({ ".git", }, { upward = true, })[1])
+-- the main journal always is "main.beancount" at the repo root
+journal_file = vim.fs.joinpath(root_dir, "main.beancount")
+
 require("lspconfig").beancount.setup({
   on_attach = helpers.keymap,
   capabilities = LSP_CAPAS,
+  root_dir = root_dir,
+  init_options = { journal_file = journal_file, },
 })
