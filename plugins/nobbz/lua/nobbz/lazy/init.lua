2a6eb57f2abffa5187298f6830d8ee411c048dde
Norbert Melzer <timmelzer@gmail.com>
2025-03-26 08:09:58 +0100

    attribute viper for their service to prepare the initial module

--- plugins/nobbz/lua/nobbz/lazy/init.lua
+++ plugins/nobbz/lua/nobbz/lazy/init.lua
@@ -1,3 +1,6 @@
+-- Huge parts of the implementation of this module has been borrowd from ViperML.
+-- https://github.com/viperML/dotfiles/tree/8fd5755078e65ce905d53fb4d72f562a4487156e/modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lazy
+
 local M = {}
 
 --- Completes the configuration by loading all specifications.
--- plugins/nobbz/lua/nobbz/lazy/specs.lua
+++ plugins/nobbz/lua/nobbz/lazy/specs.lua
@@ -1,3 +1,6 @@
+-- Huge parts of the implementation of this module has been borrowd from ViperML.
+-- https://github.com/viperML/dotfiles/tree/8fd5755078e65ce905d53fb4d72f562a4487156e/modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lazy
+
 ---This table holds all the specs for plugins to be lazy loaded.
 ---It is populated through `nobbz.lazy.add_specs` and should not be used
 ---directly.
