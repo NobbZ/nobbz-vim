686de064543fe4358385eea907b3e2fa84a7758d
Norbert Melzer <timmelzer@gmail.com>
2025-03-26 22:16:21 +0100

    more eagerloads

--- plugins/default.nix
+++ plugins/default.nix
@@ -17,6 +17,13 @@
       # mandatory plugins (aka start)
       catppuccin = false;
       lz-n = false;
+      nio = false;
+      noice = false;
+      none-ls = false;
+      notify = false;
+      nui = false;
+      plenary = false;
+      precognition = false;
       rainbow = false;
       telescope = false;
       telescope-ui-select = false;
