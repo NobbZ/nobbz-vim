893f254dab899de1b65d4b7d995feff23a27a6c8
Norbert Melzer <timmelzer@gmail.com>
2025-03-26 06:43:18 +0100

    add docstring to nobbz.lazy.specs

--- plugins/nobbz/lua/nobbz/lazy/specs.lua
+++ plugins/nobbz/lua/nobbz/lazy/specs.lua
@@ -1,3 +1,6 @@
+---This table holds all the specs for plugins to be lazy loaded.
+---It is populated through `nobbz.lazy.add_specs` and should not be used
+---directly.
 ---@type lz.n.Spec
 local M = {}
 
