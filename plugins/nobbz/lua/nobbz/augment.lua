96d94cb2763c2e7c8efef0bfe27e64fc6899612a
Norbert Melzer <timmelzer@gmail.com>
2025-03-28 22:48:18 +0100

    make augment complete work

--- plugins/nobbz/lua/nobbz/augment.lua
+++ plugins/nobbz/lua/nobbz/augment.lua
@@ -1,6 +1,17 @@
 local augment_workspace = vim.env.AUGMENT_WORKSPACE
 local augment_node = vim.env.AUGMENT_NODE
 
+---return a function to accept the augment completion
+---@param fallback string? the fallback if there is no completion available
+---@return function
+local function accept_completion(fallback)
+  if fallback then
+    return function() vim.fn["augment#Accept"](fallback) end
+  else
+    return function() vim.fn["augment#Accept"]() end
+  end
+end
+
 require("nobbz.lazy").add_specs({ {
   "augment",
   enabled = function()
@@ -9,8 +20,11 @@ require("nobbz.lazy").add_specs({ {
   before = function(plugin)
     vim.g.augment_workspace_folders = { augment_workspace, }
     vim.g.augment_node_command = augment_node
+
+    vim.g.augment_disable_tab_mapping = true
   end,
   cmd = "Augment",
+  event = "DeferredUIEnter",
 }, })
 
 WK.add({
@@ -19,4 +33,7 @@ WK.add({
   { "<leader>an", "<cmd>Augment chat-new<cr>",    desc = "Start new conversation", },
   { "<leader>at", "<cmd>Augment chat-toggle<cr>", desc = "Toggle chat panel", },
   { "<leader>as", "<cmd>Augment status<cr>",      desc = "Check Augment status", },
+  { "<C-y>",      accept_completion(),            desc = "Accept Augment completion",              mode = { "i", "s", }, },
+  { "<C-cr>",     accept_completion("\n"),        desc = "Accept Augment completion (or newline)", mode = { "i", "s", }, },
+
 })
