7b02cf1fb8a60b78dd3a5aac11a96c6782c156e5
Norbert Melzer <timmelzer@gmail.com>
2025-04-06 12:17:22 +0200

    configure neovide basics

---
+++ plugins/nobbz/lua/nobbz/neovide.lua
@@ -0,0 +1,22 @@
+vim.o.guifont = "Departure Mono:11"
+
+vim.g.neovide_scale_factor = 0.6
+
+local base_factor = 1.1
+
+local function neovide_scale_factor(factor)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * factor
+end
+
+local function scale_up()
+  neovide_scale_factor(base_factor)
+end
+
+local function scale_down()
+  neovide_scale_factor(1 / base_factor)
+end
+
+WK.add({
+  { "<C-+>", scale_up,   desc = "increase font scale", },
+  { "<C-->", scale_down, desc = "decrease font scale", },
+})
