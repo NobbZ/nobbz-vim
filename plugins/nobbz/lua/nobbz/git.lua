70facfb57f0205fab8ef93c7a741ea67f7c96d7b
Norbert Melzer <timmelzer@gmail.com>
2025-04-06 17:16:08 +0200

    additional keybinds for a better git interaction

--- plugins/nobbz/lua/nobbz/git.lua
+++ plugins/nobbz/lua/nobbz/git.lua
@@ -5,7 +5,13 @@ neogit.setup({})
 
 -- register keys for neogit
 WK.add({
-  { "<leader>g", "<cmd>:Neogit<cr>", desc = "Neogit status", },
+  { "<leader>g",   group = "git", },
+  { "<leader>gg",  "<cmd>:Neogit<cr>",                                   desc = "Neogit status", },
+  { "<leader>gb",  group = "blame", },
+  { "<leader>gbb", gitsigns.blame,                                       desc = "open file blame", },
+  { "<leader>gbi", function() gitsigns.blame_line({ full = true, }) end, desc = "show blame info", },
+  { "<leader>gbl", gitsigns.toggle_current_line_blame,                   desc = "toggle current line blame", },
+  { "<leader>gw",  gitsigns.toggle_word_diff,                            desc = "toggle word diff", },
 })
 
 gitsigns.setup()
