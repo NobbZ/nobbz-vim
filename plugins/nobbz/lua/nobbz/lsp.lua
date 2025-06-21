667ddb3de8e6989432056321bda373080dfd8281
Norbert Melzer <timmelzer@gmail.com>
2025-06-21 14:49:43 +0200

    convert gleam to new LSP abstraction

--- plugins/nobbz/lua/nobbz/lsp.lua
+++ plugins/nobbz/lua/nobbz/lsp.lua
@@ -28,7 +28,6 @@ null_ls.setup({
 -- capabilities = require("blink.cmp").get_lsp_capabilities()
 
 -- Load individual languages configuration
-require("nobbz.lsp.gleam")
 require("nobbz.lsp.html")
 require("nobbz.lsp.lua")
 require("nobbz.lsp.mdx")
@@ -48,6 +47,7 @@ local clients = {
   require("nobbz.lsp.c-cpp"),
   require("nobbz.lsp.digestif"),
   require("nobbz.lsp.elixir"),
+  require("nobbz.lsp.gleam"),
 }
 
 for _, client_config in ipairs(clients) do
--- plugins/nobbz/lua/nobbz/lsp/gleam.lua
+++ plugins/nobbz/lua/nobbz/lsp/gleam.lua
@@ -1,8 +1,6 @@
 local helpers = require("nobbz.lsp.helpers")
 
-require("lspconfig").gleam.setup({
-  on_attach = helpers.keymap,
-  capabilities = LSP_CAPAS,
-})
-
-require("nobbz.health").register_lsp("gleam")
+return {
+  name = "gleam",
+  on_attach = { helpers.keymap, },
+}
