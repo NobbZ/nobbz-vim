322085eb274543e55bb34f1c7a93d884bc41d165
Norbert Melzer <timmelzer@gmail.com>
2025-06-21 16:12:54 +0200

    convert rust to new LSP abstraction

--- plugins/nobbz/lua/nobbz/lsp/rust.lua
+++ plugins/nobbz/lua/nobbz/lsp/rust.lua
@@ -1,56 +1,56 @@
-local lspconfig = require("lspconfig")
 local helpers = require("nobbz.lsp.helpers")
 
-lspconfig.rust_analyzer.setup({
-  on_attach = helpers.combine({
-    helpers.default,
-    function(client, buffer)
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = buffer,
-        callback = function()
-          vim.lsp.buf.format({
-            async = false,
-            bufnr = buffer,
-          })
-        end,
+require("nobbz.lazy").add_specs({
+  {
+    "crates",
+    after = function()
+      require("crates").setup({
+        lsp = {
+          enabled = true,
+          on_attach = helpers.default,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
       })
     end,
-  }),
-  capabilities = LSP_CAPAS,
-  settings = {
-    ["rust-analyzer"] = {
-      inlayHints = {
-        typeHints = { enable = true, },
-        chainingHints = { enable = true, },
-        bindingModeHints = { enable = true, },
-        closureReturnTypeHints = { enable = "always", },
-        lifetimeElisionHints = { enable = "always", },
-        maxLength = 5,
-        enable = true,
-      },
-      lens = { enable = true, },
-      checkOnSave = {
-        command = "clippy",
-        allFeatures = true,
-      },
-    },
+    ft = { "toml", },
   },
 })
 
-require("nobbz.lazy").add_specs({ {
-  "crates",
-  after = function()
-    require("crates").setup({
-      lsp = {
-        enabled = true,
-        on_attach = helpers.default,
-        actions = true,
-        completion = true,
-        hover = true,
-      },
-    })
-  end,
-  ft = { "toml", },
-}, })
+local function on_attach(client, buffer)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = buffer,
+    callback = function()
+      vim.lsp.buf.format({
+        async = false,
+        bufnr = buffer,
+      })
+    end,
+  })
+end
 
-require("nobbz.health").register_lsp("rust_analyzer")
+local settings = {
+  ["rust-analyzer"] = {
+    inlayHints = {
+      typeHints = { enable = true, },
+      chainingHints = { enable = true, },
+      bindingModeHints = { enable = true, },
+      closureReturnTypeHints = { enable = "always", },
+      lifetimeElisionHints = { enable = "always", },
+      maxLength = 5,
+      enable = true,
+    },
+    lens = { enable = true, },
+    checkOnSave = {
+      command = "clippy",
+      allFeatures = true,
+    },
+  },
+}
+
+return {
+  name = "rust_analyzer",
+  on_attach = { helpers.default, on_attach, },
+  settings = settings,
+}
