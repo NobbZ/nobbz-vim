d21dd41da10f5438f0aaf305d0871a4a92bf12ef
Norbert Melzer <timmelzer@gmail.com>
2026-01-27 20:37:54 +0100

    remove dead code

--- plugins/nobbz/lua/nobbz/lazy/init.lua
+++ plugins/nobbz/lua/nobbz/lazy/init.lua
@@ -13,32 +13,6 @@ M.finish = function()
   require("lz.n").load(require("nobbz.lazy.specs"))
 end
 
----@param name string
-M.packadd = function(name)
-  vim.api.nvim_cmd({ cmd = "packadd", args = { name, }, }, {})
-end
-
----@param name string
-M.load_once = function(name)
-  if not name or type(name) ~= "string" then
-    vim.notify("Invalid plugin name provided", vim.log.levels.ERROR)
-    return
-  end
-
-  local state = require("lz.n.state").plugins
-  require("lz.n").trigger_load(name)
-
-  for k, v in ipairs(state) do
-    if v == name then
-      table.remove(state, k)
-      return
-    end
-  end
-
-  -- Only reached if `name` was not in `state`:
-  vim.notify("Plugin " .. name .. " not found in state", vim.log.levels.WARN)
-end
-
 ---@param specs lz.n.Spec[]
 M.add_specs = function(specs)
   if _finished then
